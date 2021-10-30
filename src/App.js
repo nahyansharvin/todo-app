a6d37a2d1a9f7a9a5bf74db641821c31d177d946
Nahyan Sharvin <nskonnalath@gmail.com>
2021-10-30 16:50:37 +0530

    Todo App completed

--- src/App.js
+++ src/App.js
@@ -2,19 +2,36 @@ import './App.css';
 import Styles from './style.module.css';
 import InputBox from './components/InputBox';
 import AddButton from './components/AddButton';
-import List from './components/TodoList';
+import List from './components/List';
+import {useState} from 'react';
 
 function App() {
+  const [todoList, setTodoList] = useState([]); 
+  const [todo, setTodo] = useState();
+    function addTodo(){
+      console.log('alooooo')
+        setTodoList(
+          [...todoList,
+            todo
+          ]
+        )
+        setTodo('');
+    }
+    function handleDelete(todoName){
+      setTodoList(todoList.filter((todo)=> todo !== todoName))
+      
+    }
   return (
+
     <div className={Styles.contentWrapper}>
       <div className={Styles.container}>
         <h1>To-Do-App!</h1>
         <h3>Add new To-Do</h3>
         <div className={Styles.todoSection}>
         <div className={Styles.addSection}>
-          <List/>
-          <InputBox/>
-          <AddButton/>
+          <List todoList={todoList} handleDelete={handleDelete}/>
+          <InputBox todo={todo} setTodo={setTodo}/>
+          <AddButton onClickHandler={addTodo} todo={todo}/>
         </div>
         </div>
       </div>
--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,6 +1,7 @@
 import Styles from './style.module.css'
 
-const AddButton = () =>{
-    return <button className={Styles.addButton}>Add</button>
+const AddButton = (props) =>{
+    const {onClickHandler,todo} = props;
+    return <button className={todo? Styles.addButton:Styles.addButtonMuted} onClick={todo? onClickHandler:null}>Add</button>
 }
 export default AddButton;
